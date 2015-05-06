ecb3f3cc1e0b7e2e2fdc81318f7933e5b46ac09e
Pei-Lun Lee <ypcat6@gmail.com>
2015-05-06 20:47:09 +0800

    add category and list

--- plugins/pornhub.lua
+++ plugins/pornhub.lua
@@ -1,17 +1,21 @@
 do
 
 local function run(msg, matches)
-  local data = json:decode(io.popen('timeout 30s libs/pornhub.js'):read("*all"))
+  local data = json:decode(io.popen('timeout 30s libs/pornhub.js ' .. matches[1]):read("*all"))
   if not data then
     return 'Error getting porn, please try again later.'
   else
     local receiver = get_receiver(msg)
-    send_photo_from_url(receiver, data.img)
     local txt = ''
-    txt = txt .. data.title .. '\n'
-    txt = txt .. data.hd .. ' ' .. data.duration .. '\n'
-    txt = txt .. data.views .. ' views ' .. data.rating .. '\n'
-    txt = txt .. data.link
+    if matches[1] == 'list' then
+      for k, v in pairs(data) do txt = txt .. k .. '\n' end
+    else
+      send_photo_from_url(receiver, data.img)
+      txt = txt .. data.title .. '\n'
+      txt = txt .. data.hd .. ' ' .. data.duration .. '\n'
+      txt = txt .. data.views .. ' views ' .. data.rating .. '\n'
+      txt = txt .. data.link
+    end
     return txt
   end
 end
@@ -19,10 +23,13 @@ end
 return {
   description = "Gets a random video from pornhub",
   usage = {
-    "!pornhub: Get a porn video."
+    "!pornhub: Get a porn video.",
+    "!pornhub (category): Get a porn video with given category.",
+    "!pornhub list: Get list of categories.",
   },
   patterns = {
-    "^!pornhub$"
+    "^!pornhub$",
+    "^!pornhub (.*)",
   },
   run = run
 }
